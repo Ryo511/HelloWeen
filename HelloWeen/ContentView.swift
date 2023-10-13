5bbb24e7249792596ef097b93d5a38879d0ccfdd
oliver33 <23cm0133@jec.ac.jp>
2023-10-13 09:37:38 +0900

    Initial Commit

---
+++ HelloWeen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloWeen
+//
+//  Created by OLIVER LIAO on 2023/10/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloWeen/HelloWeenApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWeenApp.swift
+//  HelloWeen
+//
+//  Created by OLIVER LIAO on 2023/10/13.
+//
+
+import SwiftUI
+
+@main
+struct HelloWeenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
